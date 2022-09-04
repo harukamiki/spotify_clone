5b56424a209b9f856b3c9affee247e6a416c6beb
Haruka Miki <haru.m1k1@keio.jp>
2022-09-04 13:27:07 +0900

    laravel9設定完了

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+  <button><</button>
+  <button>></button>
+  <div id="header-icon">
+    <select>
+      <option disabled>Haruka Miki</option>
+      <option value="icon-chooce">Account</option>
+      <option value="choix-3">Profile</option>
+      <option value="choix-4">Private session</option>
+      <option value="choix-5">Settings</option>
+      <option value="choix-5">Log out</option>
+    </select>
+  </div>
+</header>
